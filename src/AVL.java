f549b2b0ea605478410534c95017dee9ebc2a1e2
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-03-17 03:18:48 +0200

    adding rotations, balance, generic node

--- src/AVL.java
+++ src/AVL.java
@@ -1,18 +1,62 @@
-public class AVL {
-    Node root=null;
-    public void insert(int key){
+public class AVL<T extends Comparable<T>>{
+    private Node root = null;
+
+    public int height(Node node){
+        if(node == null)
+            return 0;
+        return node.getHeight();
+    }
+    public int balance(Node node){
+        if(node == null)
+            return 0;
+        return height(node.getLeft()) - height(node.getRight());
+    }
+
+    public Node leftRotate(Node node){
+        //the new root
+        Node r = node.getRight();
+        r.setLeft(node);
+        node.setRight(r.getLeft());
+        //update heights
+        node.setHeight(Math.max(height(node.getLeft()),height(node.getRight())) + 1);
+        r.setHeight(Math.max(height(r.getLeft()),height(r.getRight())) + 1);
+
+        return r;
+    }
+
+    public Node rightRotate(Node node){
+        //the new root
+        Node r = node.getLeft();
+        r.setRight(node);
+        node.setLeft(r.getRight());
+        //update heights
+        node.setHeight(Math.max(height(node.getLeft()), height(node.getRight())) + 1);
+        r.setHeight(Math.max(height(r.getLeft()),height(r.getRight())) + 1);
+
+        return r;
+    }
+    public void insert(T key){
         root=insertRec(key,root);
     }
-    public Node insertRec(int val,Node root){
+    public Node insertRec(T val,Node root){
+        //height will be updated during insertion
         if(root==null){
-            root=new Node(val);
+            root = new Node(val);
             return root;
         }
-        else if(val<root.key)
-           root.left=insertRec(val,root.left);
-        else if(val>root.key)
-           root.right=insertRec(val,root.right);
+        else if(root.getKey().compareTo(val) > 0)
+           root.setLeft(insertRec(val,root.getLeft()));
+        else if(root.getKey().compareTo(val) < 0)
+           root.setRight(insertRec(val,root.getRight()));
         return root;
     }
+
+    public Node delete(Node node){
+        return node;
+    }
+
+
+
+
 
 }
--- src/Node.java
+++ src/Node.java
@@ -1,11 +1,45 @@
-
-public class Node {
-    int key;
-    Node left,right;
-    public Node(int val){
-        key=val;
+public class Node<T extends Comparable<T>>{
+    private T key;
+    private Node<T> left,right;
+    private int height;
+    public Node(T val){
+        key = val;
         left=right=null;
+        height = 1;
     }
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public void print(){
         if(this.left!=null)
             this.left.print();
