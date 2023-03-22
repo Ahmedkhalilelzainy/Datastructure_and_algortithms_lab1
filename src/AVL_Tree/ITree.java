e5374936b94270a252ab215b3274b32858235320
abdelrahman mohamed mohamed abdelmotlb <91913879+abdelmotlb@users.noreply.github.com>
2023-03-22 14:29:11 +0200

    Delete src/AVL_Tree directory

--- src/AVL_Tree/ITree.java
+++
@@ -1,9 +0,0 @@
-package AVL_Tree;
-
-public interface ITree<T extends Comparable<T>>{
-    boolean insert(T key);
-    boolean delete(T val);
-    boolean search(T val);
-    int size();
-    int height();
-}
